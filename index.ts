5afbb4650be6f3424ce037299596d0a76603867a
vaish0317 <vaishalikatariya480@gmail.com>
2019-05-29 07:00:57 -0400

    index.ts added

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { ProductListModule } from 'src/app/product-list/product-list.module';
+
+export { ProductListComponent } from 'src/app/product-list/product-list.component';
+
+export { ProductService } from 'src/app/product.service';
+
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { ProductService } from "./app/product.service";
-export {
-  ProductListComponent
-} from "./app/product-list/product-list.component";
-export { ProductListModule } from "./app/product-list/product-list.module";
